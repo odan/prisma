ac8e6bf9830ae60141b73284892e613f0f2eb233
odan <odan@users.noreply.github.com>
2016-06-16 19:28:14 +0200

    Add null session for cli mode

--- src/Config/Service/session.php
+++ src/Config/Service/session.php
@@ -3,10 +3,20 @@
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\Storage\Handler\NativeFileSessionHandler;
 use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
+use Symfony\Component\HttpFoundation\Session\Storage\Handler\NullSessionHandler;
 
 $options = $this->config['session'];
-$storage = new NativeSessionStorage($options, new NativeFileSessionHandler());
-$session = new Session($storage);
-$session->start();
+
+if (php_sapi_name() == "cli") {
+    // In cli-mode
+    $storage = new MockArraySessionStorage(new NullSessionHandler());
+    $session = new Session($storage);
+} else {
+    // Not in cli-mode
+    $storage = new NativeSessionStorage($options, new NativeFileSessionHandler());
+    $session = new Session($storage);
+    $session->start();
+}
 
 return $session;
