f4b4a7a9ff434b4c30ab6cf63c969129736089ff
odan <odan@users.noreply.github.com>
2016-06-17 10:34:39 +0200

    Remove comments

--- bin/migration.php
+++ bin/migration.php
@@ -3,8 +3,5 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/Helper/util.php';
 
-// Load config
-// TODO
-//
 // Start phinx console
 require __DIR__ . '/../vendor/robmorgan/phinx/bin/phinx';
