f78a31cc89d9aa9584ef4710446fc45b4c77bbf5
odan <odan@users.noreply.github.com>
2017-06-25 20:06:42 +0200

    Fixed ide autocomplete

---
+++ .phpstorm.meta.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PHPSTORM_META {
+
+    $STATIC_METHOD_TYPES = [
+        \Psr\Container\ContainerInterface::get('') => [
+            '' == '@',
+        ],
+        \Slim\Container::get('') => [
+            '' == '@',
+        ],
+    ];
+}
+
+
+
+
+
--- config/routes.php
+++ config/routes.php
@@ -15,14 +15,14 @@ $app = app();
 
 // Default page
 $app->get('/', function (Request $request, Response $response) {
-    /* @var \App\Controller\HomeController $controller */
-    $controller = $this->get(\App\Controller\HomeController::class);
-    return $controller->indexPage($request, $response);
+    /** @var $this \Slim\Container */
+   $controller = $this->get(\App\Controller\HomeController::class);
+   return $controller->indexPage($request, $response);
 });
 
 // Json request
 $app->post('/index/load', function (Request $request, Response $response) {
-    /* @var \App\Controller\HomeController $controller */
+    /** @var $this \Slim\Container */
     $controller = $this->get(\App\Controller\HomeController::class);
     return $controller->load($request, $response);
 });
@@ -31,40 +31,40 @@ $app->post('/index/load', function (Request $request, Response $response) {
 // No auth check for this actions
 // Option: _auth = false (no authentication and authorization)
 $app->post('/login', function (Request $request, Response $response) {
-    /* @var \App\Controller\LoginController $controller */
+    /** @var $this \Slim\Container */
     $controller = $this->get(\App\Controller\LoginController::class);
     return $controller->loginSubmit($request, $response);
 })->setArgument('_auth', false);
 
 $app->get('/login', function (Request $request, Response $response) {
-    /* @var \App\Controller\LoginController $controller */
+    /** @var $this \Slim\Container */
     $controller = $this->get(\App\Controller\LoginController::class);
     return $controller->loginPage($request, $response);
 })->setArgument('_auth', false)->setName('login');
 
 $app->get('/logout', function (Request $request, Response $response) {
-    /* @var \App\Controller\LoginController $controller */
+    /** @var $this \Slim\Container */
     $controller = $this->get(\App\Controller\LoginController::class);
     return $controller->logout($request, $response);
 })->setArgument('_auth', false);
 
 // Users
 $app->get('/users', function (Request $request, Response $response) {
-    /* @var \App\Controller\UserController $controller */
+    /** @var $this \Slim\Container */
     $controller = $this->get(\App\Controller\UserController::class);
     return $controller->indexPage($request, $response);
 });
 
 // This route will only match if {id} is numeric
 $app->get('/users/{id:[0-9]+}', function (Request $request, Response $response) {
-    /* @var \App\Controller\UserController $controller */
+    /** @var $this \Slim\Container */
     $controller = $this->get(\App\Controller\UserController::class);
     return $controller->editPage($request, $response);
 });
 
 // Sub-Resource
 $app->get('/users/{id:[0-9]+}/reviews', function (Request $request, Response $response, $args) {
-    /* @var \App\Controller\UserController $controller */
+    /** @var $this \Slim\Container */
     $controller = $this->get(\App\Controller\UserController::class);
     return $controller->reviewPage($request, $response, $args);
 });
