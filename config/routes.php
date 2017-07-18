4824beed9c21ff312c6c2d60edd1b8b48485eea7
odan <odan@users.noreply.github.com>
2017-07-18 19:32:37 +0200

    Simple routes

--- config/routes.php
+++ config/routes.php
@@ -1,8 +1,5 @@
 <?php
 
-use Slim\Http\Request;
-use Slim\Http\Response;
-
 /**
  * Define the Slim application routes
  *
@@ -14,49 +11,23 @@ use Slim\Http\Response;
 $app = app();
 
 // Default page
-$app->get('/', function (Request $request, Response $response) {
-    /** @var $this \Slim\Container */
-    return $this->get(\App\Controller\HomeController::class)->indexPage($request, $response);
-});
+$app->get('/', 'App\Controller\HomeController:indexPage');
 
 // Json request
-$app->post('/index/load', function (Request $request, Response $response) {
-    /** @var $this \Slim\Container */
-    return $this->get(\App\Controller\HomeController::class)->load($request, $response);
-});
+$app->post('/index/load', 'App\Controller\HomeController:load');
 
 // Login
 // No auth check for this actions
 // Option: _auth = false (no authentication and authorization)
-$app->post('/login', function (Request $request, Response $response) {
-    /** @var $this \Slim\Container */
-    return $this->get(\App\Controller\LoginController::class)->loginSubmit($request, $response);
-})->setArgument('_auth', false);
-
-$app->get('/login', function (Request $request, Response $response) {
-    /** @var $this \Slim\Container */
-    return $this->get(\App\Controller\LoginController::class)->loginPage($request, $response);
-})->setArgument('_auth', false)->setName('login');
-
-$app->get('/logout', function (Request $request, Response $response) {
-    /** @var $this \Slim\Container */
-    return $this->get(\App\Controller\LoginController::class)->logout($request, $response);
-})->setArgument('_auth', false);
+$app->post('/login', 'App\Controller\LoginController:loginSubmit')->setArgument('_auth', false);
+$app->get('/login', 'App\Controller\LoginController:loginPage')->setArgument('_auth', false)->setName('login');
+$app->get('/logout', 'App\Controller\LoginController:logout')->setArgument('_auth', false);
 
 // Users
-$app->get('/users', function (Request $request, Response $response) {
-    /** @var $this \Slim\Container */
-    return $this->get(\App\Controller\UserController::class)->indexPage($request, $response);
-});
+$app->get('/users', 'App\Controller\UserController:indexPage');
 
 // This route will only match if {id} is numeric
-$app->get('/users/{id:[0-9]+}', function (Request $request, Response $response) {
-    /** @var $this \Slim\Container */
-    return $this->get(\App\Controller\UserController::class)->editPage($request, $response);
-});
+$app->get('/users/{id:[0-9]+}', 'App\Controller\UserController:editPage');
 
 // Sub-Resource
-$app->get('/users/{id:[0-9]+}/reviews', function (Request $request, Response $response, $args) {
-    /** @var $this \Slim\Container */
-    return $this->get(\App\Controller\UserController::class)->reviewPage($request, $response, $args);
-});
+$app->get('/users/{id:[0-9]+}/reviews', 'App\Controller\UserController:reviewPage');
